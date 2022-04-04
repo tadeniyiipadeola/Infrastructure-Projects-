d1f44b8bbb7feadfa5f20411016f42fc73d7e017
Temi <stadeniy@cougarnet.uh.edu>
2022-04-03 23:36:31 -0500

    first commit

--- lib/simple-app1-stack.ts
+++ lib/simple-app1-stack.ts
@@ -1,11 +1,15 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 
 export class SimpleApp1Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const bucket = new Bucket(this, 'MySimpleApp1Bucket',{
+      encryption: BucketEncryption.S3_MANAGED,
+    });
     // The code that defines your stack goes here
 
     // example resource
