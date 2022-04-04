fe2e2a65fc071195a75ab88280358f6af798820c
Temi <stadeniy@cougarnet.uh.edu>
2022-04-04 00:38:10 -0500

    left here with an error with constructor not sure why could be library or syntax

--- lib/simple-app1-stack.ts
+++ lib/simple-app1-stack.ts
@@ -1,20 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
+import * as cdk from '@aws-cdk/core';
 
 export class SimpleApp1Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const bucket = new Bucket(this, 'MySimpleApp1Bucket',{
+    const bucket = new Bucket(this, 'MySimpleApp1Bucket',
+    {
       encryption: BucketEncryption.S3_MANAGED,
     });
-    // The code that defines your stack goes here
+    new cdk.CfnOutput(this, 'MySimpleApp1BucketNameExport', 
+    {
+      value: bucket.bucketName,
+      exportName: 'MySimpleApp1BucketName',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleApp1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
